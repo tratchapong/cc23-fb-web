e018f87f626ae8c57f7ac0d87cb88c416bedda5e
tratchapong <tratchapong@gmail.com>
2026-08-05 17:42:18 +0700

    Header with icon svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { FacebookLogo, HomeIcon } from '@/icons'
 import useUserStore from '../stores/userStore'
 
 function Header() {
  const logout = useUserStore(state => state.logout)
   return (
-   <div className='text-4xl py-4 border flex gap-4'>Header
+   <div className='text-4xl py-4 border flex gap-4'>
+		 <FacebookLogo className='w-16' />
+		 <HomeIcon className='w-16' />
      <button className='btn btn-primary' onClick={logout}>Logout</button>
    </div>
  )}
