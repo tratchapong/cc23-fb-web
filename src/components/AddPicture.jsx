e64edddcdfb5e60de383238d3de0b04729b4c756
tratchapong <tratchapong@gmail.com>
2026-08-06 13:54:23 +0700

    AddPic start

---
+++ src/components/AddPicture.jsx
@@ -0,0 +1,8 @@
+function AddPicture() {
+ return (
+   <div>
+     <p className='text-4xl'>AddPicture Mocked..</p>
+   </div>
+ )}
+
+export default AddPicture
--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { useState } from 'react'
 import Avatar from './Avatar'
 import useUserStore from '@/stores/userStore'
 import { PhotoIcon2 } from '@/icons'
+import AddPicture from './AddPicture'
 
 function PostForm() {
  const user = useUserStore(state => state.user)
+ const [addPic, setAddPic] = useState(false)
+
  return (
    <div className='flex flex-col gap-2'>
      <h3 className="text-xl text-center">Create post</h3>
@@ -25,10 +28,13 @@ function PostForm() {
      <textarea className='textarea textarea-ghost w-full'
        placeholder={`what do you think? ${user.firstName}`}
      ></textarea>
+     {addPic && <AddPicture />}
      <div className="flex border rounded-lg p-2 justify-between items-center">
        <p>add with your post</p>
        <div className="flex justify-center items-center w-10 h-10 rounded-full bg-slate-100
-         hover:bg-slate-200 active:scale-110">
+         hover:bg-slate-200 active:scale-110"
+         onClick = {()=>setAddPic(prv=>!prv)}
+       >
          <PhotoIcon2 className='w-7' />
        </div>
      </div>
